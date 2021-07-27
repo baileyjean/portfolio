23643b86e72db8e574cba59cb06a2bd53b25d2a6
Bailey Jean <79871279+baileyjean@users.noreply.github.com>
2021-07-27 10:12:23 -0700

    Add sexy social links instead of text to the Connect page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import bailey from '../styles/images/happyBailey.jpg'
+import github from '../styles/images/github.png'
+import linkedin from '../styles/images/linkedin.png'
+import emailMe from '../styles/images/emailMe.png'
 
 const Contact = () => {
 
@@ -7,10 +10,12 @@ const Contact = () => {
     <div className="pages">
       <div className="contact-page">
         <h1>Let's Connect!</h1>
-        <img src={bailey} alt=""/>
+        <img src={bailey} alt="Me being absolutely awesome at the Sanford Underground Research Facility"/>
         <br />
         <span>
-          <a href="https://www.linkedin.com/in/baileyleavitt/">LinkedIn</a> | <a href="mailto: baileyjleavitt@gmail.com">Email</a> | <a href="https://github.com/baileyjean">GitHub</a>
+          <a href="https://www.linkedin.com/in/baileyleavitt/"><img src={linkedin} alt="linkedin profile" style={{width: "4em"}} /></a>
+          <a href="mailto: baileyjleavitt@gmail.com"><img src={emailMe} alt="email me here" style={{width: "4em"}} /></a>
+          <a href="https://github.com/baileyjean"><img src={github} alt="check out my GIT repos here" style={{width: "4em"}} /></a>
         </span>
       </div>
     </div>
